7434675d8f5380dbeec11906cc032dda3a87861d
dylan-yoon <d.dylany21@gmail.com>
2023-10-23 15:40:05 +0900

    feat: YagomData 구현 및 셀적용

--- webCrawling/webCrawling/ViewController.swift
+++ webCrawling/webCrawling/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // 기본 셀
+    var yagomDatas = [YagomData(name: "Yagom", position: "대장", image: UIImage(named: "yagomlogo")!)]
+    
     @IBOutlet weak var mainTableView: UITableView!
     
     override func viewDidLoad() {
@@ -27,11 +30,17 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return yagomDatas.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell") as? TableViewCell else { return UITableViewCell() }
+        
+        let data = yagomDatas[indexPath.row]
+        
+        cell.nameLabel.text = data.name
+        cell.positionLabel.text = data.position
+        cell.photoImageView.image = data.image
         
         return cell
     }
---
+++ webCrawling/webCrawling/YagomData.swift
@@ -0,0 +1,14 @@
+//
+//  YagomData.swift
+//  webCrawling
+//
+//  Created by Dylan_Y on 2023/10/23.
+//
+
+import UIKit
+
+struct YagomData {
+    var name: String
+    var position: String
+    var image: UIImage
+}
