e7bc8cb4b1365bfcc227b8df75ac42e5f396dcdf
dylan-yoon <d.dylany21@gmail.com>
2023-11-01 17:51:39 +0900

    refactor: 네이밍 변경

--- webCrawling/webCrawling/NetWork.swift
+++ webCrawling/webCrawling/NetWork.swift
@@ -37,32 +37,32 @@ class NetWork {
         dataTask.resume()
     }
     
-    private func webCrawlingData(_ dataString: String) -> [YagomData] {
+    private func webCrawlingData(_ html: String) -> [YagomData] {
         var yagomDatas = [YagomData]()
         
         do {
-            
-            let doc: Document = try SwiftSoup.parse(dataString)
-            
+            let doc: Document = try SwiftSoup.parse(html)
             let firstDivergingPoint: Elements = try doc.select("div.css-1r6553s")
             let secondDivergingPoint: Elements = try firstDivergingPoint.select("div.css-lwlrzb")
             
             //MARK: Name 가져오기
             let nameElements: Elements = try secondDivergingPoint.select("div > h4 > span")
             
-            for (index, i) in nameElements.enumerated() {
+            for (index, nameElement) in nameElements.enumerated() {
                 print(index, terminator: " ")
-                print(try i.text())
-                yagomDatas.append(YagomData(name: try i.text(), position: ""))
+                print(try nameElement.text())
+                yagomDatas.append(YagomData(name: try nameElement.text(), position: ""))
             }
             
             //MARK: Position 가져오기
+//            let positionElements: Elements = try secondDivergingPoint.select("div > span") 더 다양한 데이터들이 들어옴
+            
             let positionElements: Elements = try secondDivergingPoint.select("div.css-gbfsct").select("div > span")
             var positionData = [String]()
-            for (index, i) in positionElements.enumerated() {
+            for (index, positionElement) in positionElements.enumerated() {
                 print(index, terminator: " ")
-                positionData.append(try i.text())
-                print(try i.text())
+                positionData.append(try positionElement.text())
+                print(try positionElement.text())
             }
             
             // 불필요한 데이터 삭제 -> [5, 7, 9, 16, 17, 34, 35, 43]
@@ -75,25 +75,12 @@ class NetWork {
                 yagomDatas[index].position = value
             }
             
-            
-//            //MARK: Position 가져오기
-//            let positionElements: Elements = try secondDivergingPoint.select("div > span")
-//            for (index, i) in positionElements.enumerated() {
-//
-//                if index % 2 == 0 {
-//                    print(index, terminator: " ")
-//                    print(try i.text())
-////                    yagomDatas[index/2].position = try i.text()
-//                }
-//
-//            }
-            
             //MARK: ImageLink 가져오기
             let imageElements: Elements = try firstDivergingPoint.select("div.css-1ts4rwa > div > div > div > div > div > div > div").select("img")
-            for (index, i) in imageElements.enumerated() {
+            for (index, elementValue) in imageElements.enumerated() {
                 print(index, terminator: " ")
-                print(try i.attr("src".description))
-                yagomDatas[index].imageLink = try i.attr("src".description)
+                print(try elementValue.attr("src".description))
+                yagomDatas[index].imageLink = try elementValue.attr("src".description)
             }
         } catch {
             print(error.localizedDescription)
